ca7aaba0c3dbb751db45b1e2baad9b0bf965715e
rifansetiawan <mrifansetiawan@gmail.com>
2021-07-04 21:26:32 +0700

    edit

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -1,2 +1,2 @@
-{{-- <link rel="stylesheet" href="../../../public/css/app.css">
-<script src="../../../public/css/app.css" defer></script> --}}
+<link rel="stylesheet" href="../../../public/css/app.css">
+{{-- <script src="../../../public/css/app.css" defer></script> --}}
