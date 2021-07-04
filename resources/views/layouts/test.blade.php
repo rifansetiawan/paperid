487fccd2acbf59d3dcdfdcb25efc1ec65d32125e
rifansetiawan <mrifansetiawan@gmail.com>
2021-07-04 21:29:28 +0700

    manual script

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="../../../public/css/app.css">
-<script src="../../../public/css/app.css" defer></script>
+<link rel="stylesheet" href="http://51.89.142.185/paperid/public/css/app.css">
+<script src="http://51.89.142.185/paperid/public/js/app.js" defer></script>
